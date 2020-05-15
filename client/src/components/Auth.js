60da6a83d09bafec843d8c180fa19e81420ab8ae
Cassandra Heil <Cassandraheil19@gmail.com>
2020-05-15 16:25:29 -0400

    got yelp api working and showing data, also restructed user a bit

--- client/src/components/Auth.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-
-class user extends Component{
-    state
-
-}
--- client/src/index.js
+++ client/src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security, ImplicitCallback } from '@okta/okta-react';
 
 ReactDOM.render(
-  <Router>
-    <Security
-      issuer={`${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`}
-      client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
-      redirect_uri={`${window.location.origin}/implicit/callback`}
-    >
-      <Route path="/" exact component={App} />
-      <Route path="/implicit/callback" component={ImplicitCallback} />
-    </Security>
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
