9fde4e5fb5ed9b1105fb671e7adb2c47ea950c39
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-05-26 20:48:19 -0400

    about page styling

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../Grid";
 import { List, ListItem } from "../List";
 import {Input, TextArea, FormBtn } from "../Form";
-// import "./style.css";
+import "./style.css";
 
 class About extends Component {
 
     render() {
         return (
             <Container fluid>
-                       <div className="jumbotron mt-4 jcolor scrollbar scrollbar-primary">
+                       <div className="jumbotron mt-4 jcolor">
           <div class="row">
             <div class="col-2">
               <img alt="Kathy" src="https://i.imgur.com/YRh15Mk.png" class="kathy"></img></div>
@@ -25,11 +25,11 @@ class About extends Component {
                 <h4>
                     Kathy is currently working on bringing you reviews for the best 
                     events and nightlife in your area. Stay tuned!
-                </h4>
+                </h4><br/>
                 <h4>Sign up for an account today to get all the latest and greatest recommendations for your area!</h4> 
-                        <Link to="/register">Register</Link>
+                        <button className="btn btn-outline-dark"><a href="/register">Register</a></button><br/><br/>
                         <h4>Already have an account?</h4>
-                        <Link to ="/login">Login</Link>
+                       <button className="btn btn-outline-dark"> <Link to ="/login">Login</Link></button>
             </div>
        
 
