7e84de796100ba719b4d1d3bf2cf9ebe86fefb03
Cassandra Heil <Cassandraheil19@gmail.com>
2020-05-30 13:09:50 -0400

    merge

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,19 +2,14 @@ import axios from "axios";
 
 export default {
   getWeather: function (coordinates) {
+    console.log("API page lat, lon: ", coordinates)
     return axios.post("/api/weather", coordinates)
   },
   getPosts: function(location) {
     return axios.get("/api/posts/" + location);
   },
   locationLookUp: function() {
-   return axios.get("http://api.ipstack.com/check?access_key=e8dc22e36bb38cce9e75705776e3ca42&format=1")
-    .then(res => {
-      console.log("res.data", res.data)
-      return res.data
-    }
-       
-      )
+    return axios.get("/api/location");
   },
   yelpCall: function(location) {
     return axios.post("/api/restaurants", { location })
@@ -39,5 +34,9 @@ export default {
   },
   getUser: function(userID) {
     return axios.post("api/users/info/" + userID)
-  }
+  },
+
+  //updateLocation: function(userID, location) {
+    // return axios.put("api/NewRoute", { userLocation: location, userID })
+  //}
 };
