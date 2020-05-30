1c25bebb51a138c82ce43f54cd5307ee48df7622
Cassandra Heil <Cassandraheil19@gmail.com>
2020-05-30 09:50:26 -0400

    moved location to front end to pull from browser

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,7 +9,13 @@ export default {
     return axios.get("/api/posts/" + location);
   },
   locationLookUp: function() {
-    return axios.get("/api/location");
+   return axios.get("http://api.ipstack.com/check?access_key=e8dc22e36bb38cce9e75705776e3ca42&format=1")
+    .then(res => {
+      console.log("res.data", res.data)
+      return res.data
+    }
+       
+      )
   },
   yelpCall: function(location) {
     return axios.post("/api/restaurants", { location })
